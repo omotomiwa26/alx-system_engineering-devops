b9a2265b579af01a67b1f3869b02acf2eb4cd6e5
omotomiwa26 <omotomiwa26@gmail.com>
2023-12-22 22:59:07 +0000

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,26 @@
-# This file installs flask from pip3
+# This puppet manifest file installs flask from pip3
 
+# Install Python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# Install pip3
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
 
