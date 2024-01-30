89966324a312bc5b19ea44ab5f635d9d0cc5d988
omotomiwa26 <omotomiwa26@gmail.com>
2024-01-30 03:37:23 +0000

    cript that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,15 +12,22 @@ from sys import argv, exit
 REST_API_URL = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
-    if len(argv) > 1:
-        if argv[1].replace(".", "", 1).isdigit():
-            id = int(argv[1])
-            response = r.get(F'{REST_API_URL}/users/{id}').json()
-            task_request = r.get(F'{REST_API_URL}/todos').json()
-            emp_name = response.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_request))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(F'Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):')
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print(F'\t {task.get("title")}')
+    if len(argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        exit(1)
+    employee_id = argv[1]
+    if not employee_id.replace(".", "", 1).isdigit():
+        print("Error: Employee ID must be a valid integer.")
+        exit(1)
+    employee_id = int(argv[1])
+    response = r.get(F'{REST_API_URL}/users/{id}').json()
+    task_request = r.get(F'{REST_API_URL}/todos').json()
+    emp_name = response.get('name')
+    tasks = list(filter(lambda x: x.get('userId') == employee_id,
+                        task_request))
+    completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+    print(F'Employee {emp_name} is done with tasks '
+          F'({len(completed_tasks)}/{len(tasks)}):')
+    if len(completed_tasks) > 0:
+        for task in completed_tasks:
+            print(F'\t {task.get("title")}')
