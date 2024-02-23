90046cdaa043297d76db738cd7de5e3f2785f882
omotomiwa26 <omotomiwa26@gmail.com>
2024-02-23 04:38:25 +0000

    Change the OS configuration

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,14 @@
 # This puppet script Changes the OS configuration file so that it is possible to login with the holberton user and open a file without any error message.
 
-exec {'Change OS security config':
-  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+#exec {'Change OS security configuration':
+#  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+#  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+#}
+
+exec {'Change OS security configuration':
+command => "bash -c \"sed -iE 's/^holberton hard nofile \
+5/holberton hard nofile 88888/' /etc/security/limits.conf; \
+sed -iE 's/^holberton soft nofile \
+4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
